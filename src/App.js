2ab710dfb24af8d82ef9eaa53194911b7674071c
Vaishak Kallore <vaishak.kallore@detrans.io>
2020-05-21 12:11:20 +0200

    Reports with custom styles and fields

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Admin, Resource, ListGuesser } from "react-admin"
+import jsonServerProvider from "ra-data-json-server"
+import polyglotI18nProvider from "ra-i18n-polyglot"
+import arabicMessages from "ra-language-arabic"
+import PostsIcon from "@material-ui/icons/ListSharp"
+import UsersIcon from "@material-ui/icons/People"
+import { UserList } from "./users"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const i18nProvider = polyglotI18nProvider(() => arabicMessages, "ar")
+
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com")
+const App = () => (
+  <Admin dataProvider={dataProvider} i18nProvider={i18nProvider}>
+    <Resource name="posts" list={ListGuesser} icon={PostsIcon} />
+    <Resource name="users" list={UserList} icon={UsersIcon} />
+  </Admin>
+)
+
+export default App
---
+++ src/users/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { List, Datagrid, TextField, EmailField } from "react-admin"
+import MyUrlField from '../components/MyUrlField'
+
+export const UserList = (props) => {
+  // const translate = useTranslate()
+  return (
+    <List perPage={4} {...props}>
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <TextField source="name" />
+        {/* <TextField source="username" /> */}
+        <EmailField source="email" />
+        {/* <TextField source="address.street" /> */}
+        <TextField source="phone" />
+        <MyUrlField source="website" label="User website" />
+        <TextField source="company.name" />
+      </Datagrid>
+    </List>
+  )
+}
