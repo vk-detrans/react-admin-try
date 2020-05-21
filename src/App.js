628ef9121fc787afc427438d08a95aba31c3f900
Vaishak Kallore <vaishak.kallore@detrans.io>
2020-05-21 13:17:07 +0200

    Custom posts listing

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import polyglotI18nProvider from 'ra-i18n-polyglot'
 import arabicMessages from 'ra-language-arabic'
 import PostsIcon from '@material-ui/icons/ListSharp'
 import UsersIcon from '@material-ui/icons/People'
+
 import { UserList } from './users'
+import { PostList } from './posts'
 
 
 const i18nProvider = polyglotI18nProvider(() => arabicMessages, 'ar')
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 const App = () => (
-  <Admin dataProvider={dataProvider} i18nProvider={i18nProvider}>
-    <Resource name='posts' list={ListGuesser} icon={PostsIcon} />
+    <Resource name='posts' list={PostList} icon={PostsIcon} />
     <Resource name='users' list={UserList} icon={UsersIcon} />
   </Admin>
 )
